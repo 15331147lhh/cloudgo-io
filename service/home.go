2ce0cf492e037af7f6c28c52410cfece62764448
elevenistaken <1726040226@qq.com>
2017-11-23 20:59:43 +0800

    pictures

---
+++ service/home.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+func homeHandler(formatter *render.Render) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.HTML(w, http.StatusOK, "index", struct {
+			ID      string `json:"id"`
+			Content string `json:"content"`
+		}{ID: "198273  ", Content: "Hello from Go !!!"})
+	}
+
+}
